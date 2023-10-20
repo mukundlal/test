8ffd39d46ec0037836a0aab004ece1bb190ab3a1
Mukund Lal <mukundlaltnr@gmail.com>
2023-10-20 21:53:49 +0530

    Initial Commit

---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Mukund Lal on 20/10/23.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
