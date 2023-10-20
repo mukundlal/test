6ab1147594a36e31ea69e391a81b1388d6fec061
Mukund Lal <mukundlaltnr@gmail.com>
2023-10-20 22:16:33 +0530

    testing swift ui

--- test/ContentView.swift
+++ test/ContentView.swift
@@ -8,15 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var number1: String = ""
+        @State private var number2: String = ""
+        @State private var result: Int = 0
+
+        var body: some View {
+            VStack {
+                Image(systemName: "swift")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                Text("SwiftUI Example")
+                    .font(.title3)
+                    .padding()
+
+                TextField("Enter number 1", text: $number1)
+                    .padding()
+                    .keyboardType(.numberPad)
+                TextField("Enter number 2", text: $number2)
+                    .padding()
+                    .keyboardType(.numberPad)
+
+                Button("Add Numbers") {
+                    if let num1 = Int(number1), let num2 = Int(number2) {
+                        result = num1 + num2
+                    }
+                }
+                .padding()
+                .foregroundColor(.white)
+                .background(Color.black)
+                .cornerRadius(20)
+
+                if result != 0 {
+                    Text("Result: \(result)")
+                        .padding()
+                }
+            }
+            .padding()
         }
-        .padding()
-    }
 }
 
 #Preview {
